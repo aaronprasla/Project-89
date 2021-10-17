69a562e40a03978b19bb5754815142fedf89b4d3
aaronprasla <52900151+aaronprasla@users.noreply.github.com>
2021-10-16 20:54:43 -0500

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,14 @@
+function Send() {
+    number1 = document.getElementById('number1').value;
+    number2 = document.getElementById('number2').value;
+
+    actual_number = parseInt(number1) * parseInt(number2);
+
+    question_number = '<h4>' + number1 + "X" + number2 + '</h4>';
+    input_box = '<br>Answer: <input type="text" id="input_check_box">';
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_number + input_box + check_button;
+    document.getElementById('output').innerHTML = row;
+    document.getElementById('number1').innerHTML = '';
+    document.getElementById('number2').innerHTML = '';
+}
